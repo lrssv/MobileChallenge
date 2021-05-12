36b27a8dea1a4e1b55d5bfe7656dc6cea101bc28
Larissa Silva <larissa.silva@gerencianet.com.br>
2021-05-12 09:14:02 -0300

    Initial Commit

---
+++ MobileChallenge/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MobileChallenge
+//
+//  Created by Larissa Silva | Gerencianet on 10/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfTest: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tfTest?.font = UIFont(name: "BebasNeueRegular", size: 35)
+    }
+    
+
+
+}
+
